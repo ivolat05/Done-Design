68875e059ef3b58208635d207df1c9a207137a8c
andrei_labadaev <andrei-labadaev@mail.ru>
2022-02-03 22:12:31 +0300

    uppdate 22.12

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -66,6 +66,7 @@ $(function () {
 
             function selectToggle() {
                 this.parentElement.classList.toggle('is-active');
+                this.parentElement.parentElement.classList.toggle('active');
             }
 
             function selectChoose() {
@@ -73,7 +74,9 @@ $(function () {
                 let select = this.closest('.select');
                 let currentText = select.querySelector(".select__current");
                 currentText.innerText = text;
+                currentText.value = text;
                 select.classList.remove('is-active');
+                select.parentElement.classList.remove('active');
             }
         }
 
@@ -165,6 +168,7 @@ $(function () {
     let listCountry = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
     autocomplete("address", listCountry);
     autocomplete("location", listCountry);
+
     function autocomplete(classInputSearch, arr) {
         /*the autocomplete function takes two arguments,
         the text field element and an array of possible autocompleted values:*/
@@ -175,7 +179,11 @@ $(function () {
             inp.addEventListener("input", function (e) {
                 let a, b, i, val = this.value;
                 /*close any already open lists of autocompleted values*/
+
+                inp.parentElement.classList.add('active')
                 closeAllLists();
+                // добовление класса к родительскому блоку
+                inp.parentElement.classList.add('active')
                 if (!val) { return false; }
                 currentFocus = -1;
                 /*create a DIV element that will contain the items (values):*/
@@ -257,10 +265,13 @@ $(function () {
                         x[i].parentNode.removeChild(x[i]);
                     }
                 }
+
             }
             /*execute a function when someone clicks in the document:*/
             document.addEventListener("click", function (e) {
                 closeAllLists(e.target);
+                // удалене класса у родительскому блоку
+                inp.parentElement.classList.remove('active')
             });
         }
     }
@@ -414,33 +425,56 @@ $(function () {
 
     function mapFixed() {
         let mapId = document.getElementById('map');
-        let offsetHeader = document.querySelector('.header').offsetHeight;
-        let offsetSearchBox = document.querySelector('.search-box').offsetHeight;
-        let offsetFilter = document.querySelector('.filter').offsetHeight;
-        let summHead = offsetHeader + offsetSearchBox + offsetFilter;
-        window.addEventListener('scroll', () => {
-            let scrollDistanse = window.scrollY;
-            let pageHeight = document.querySelector('.page').offsetHeight;
-            let footerHeight = document.querySelector(".footer").offsetHeight;
-            let stopMap = pageHeight - 780 - footerHeight;
-            if (scrollDistanse >= summHead) {
-                mapId.style.position = 'fixed';
-            }
-            if (stopMap <= scrollDistanse) {
-                mapId.style.top = 'auto'
-                mapId.style.position = 'absolute';
-                mapId.style.bottom = '40px';
-            } else if (stopMap >= scrollDistanse) {
-                mapId.style.top = '0'
-                mapId.style.bottom = 'auto';
-                mapId.style.position = 'fixed';
-            }
-            if (scrollDistanse < summHead) {
-                mapId.style.position = 'absolute';
-            }
-        })
+        if (mapId) {
+            let offsetHeader = document.querySelector('.header').offsetHeight;
+            let offsetSearchBox = document.querySelector('.search-box').offsetHeight;
+            let offsetFilter = document.querySelector('.filter').offsetHeight;
+            let summHead = offsetHeader + offsetSearchBox + offsetFilter;
+
+            window.addEventListener('scroll', () => {
+                let scrollDistanse = window.scrollY;
+                let pageHeight = document.querySelector('.page').offsetHeight;
+                let footerHeight = document.querySelector(".footer").offsetHeight;
+                let stopMap = pageHeight - 780 - footerHeight;
+                if (scrollDistanse >= summHead) {
+                    mapId.style.position = 'fixed';
+                }
+                if (stopMap <= scrollDistanse) {
+                    mapId.style.top = 'auto'
+                    mapId.style.position = 'absolute';
+                    mapId.style.bottom = '40px';
+                } else if (stopMap >= scrollDistanse) {
+                    mapId.style.top = '0'
+                    mapId.style.bottom = 'auto';
+                    mapId.style.position = 'fixed';
+                }
+                if (scrollDistanse < summHead) {
+                    mapId.style.position = 'absolute';
+                }
+            })
+        }
     }
 
     mapFixed();
+
+    // reset player filster
+
+    function playerResetBtn() {
+        let playerBtn = document.querySelector('.player__reset-btn');
+        if (playerBtn) {
+            playerBtn.addEventListener('click', () => {
+                let selectCurrent = document.querySelectorAll('.select__current');
+                let mainFormInput = document.querySelectorAll('.main-form-input');
+                for (let i = 0; i < selectCurrent.length; i++) {
+                    selectCurrent[i].value = '';
+                }
+                for (let i = 0; i < mainFormInput.length; i++) {
+                    mainFormInput[i].value = '';
+                }
+            })
+
+        }
+    }
+    playerResetBtn();
 })
 
